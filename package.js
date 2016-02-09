2c6a5c7d1827ed7ebd46036918e58bc5561045c6
Tamino Martinius <dev@zaku.eu>
2016-02-09 10:07:44 +0100

    Bumped package versions and changed package name

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: "tamino-martinius:smart-record",
+  name: "zaku:smart-record",
   summary: "Rails like models using ES6.",
   version: "0.0.2",
   git: "https://github.com/tamino-martinius/meteor-smart-record.git"
@@ -12,8 +12,8 @@ cs = [c, s];
 Package.onUse(function (api) {
   api.versionsFrom("METEOR@1.2.1");
   api.use('mongo'                                   , cs);
-  api.use('dburles:mongo-collection-instances'      , cs);
-  api.use('dburles:eslint'                          , cs);
+  api.use('dburles:mongo-collection-instances@0.1.3', cs);
+  api.use('dburles:eslint@1.0.1'                    , cs);
   api.use('underscorestring:underscore.string@3.2.3', cs);
   api.use('underscore'                              , cs);
   api.use('ecmascript'                              , cs);
@@ -28,8 +28,8 @@ Package.onTest(function (api) {
   api.use('tinytest'                                ,  s);
   api.use('ecmascript'                              ,  s);
   api.use('underscore'                              ,  s);
-  api.use('dburles:eslint'                          ,  s);
-  api.use('tamino-martinius:smart-record'           ,  s);
+  api.use('dburles:eslint@1.0.1'                    ,  s);
+  api.use('zaku:smart-record'                       ,  s);
 
   api.add_files('tests/models.js'                   ,  s);
   api.add_files('tests/smart_model_tests.js'        ,  s);
