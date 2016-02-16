92b33a319caa4a2a25e32910887426ddf336c6a9
Tamino Martinius <dev@zaku.eu>
2016-02-16 13:55:45 +0100

    Removed unused file

--- examples/autoform-demo/common/2_collections.js
+++
@@ -1,7 +0,0 @@
-var Collections = {
-  Person: Person.collection(),
-  Item: Item.collection(),
-  PersonWithContacts: PersonWithContacts.collection(),
-};
-
-Meteor.isClient && Template.registerHelper("Collections", Collections);
