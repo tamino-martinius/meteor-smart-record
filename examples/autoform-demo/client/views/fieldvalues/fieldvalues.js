0bb0896cef3c0d8dcffdff6274896a174c53773f
Tamino Martinius <dev@zaku.eu>
2016-02-16 11:41:52 +0100

    Updated field values example to work with smart record

--- examples/autoform-demo/client/views/fieldvalues/fieldvalues.js
+++ examples/autoform-demo/client/views/fieldvalues/fieldvalues.js
@@ -1,29 +1,35 @@
-FieldValueIs = new Mongo.Collection("FieldValueIs");
-FieldValueIs.attachSchema(new SimpleSchema({
-  a: {
-    type: String,
-    allowedValues: ["foo", "bar"]
-  },
-  b: {
-    type: String
-  },
-  c: {
-    type: [String],
-    minCount: 1,
-    maxCount: 3
+FieldValueIs = class FieldValueIs extends SmartModel {
+  static schema() {
+    return {
+      a: {
+        type: String,
+        allowedValues: ["foo", "bar"]
+      },
+      b: {
+        type: String
+      },
+      c: {
+        type: [String],
+        minCount: 1,
+        maxCount: 3
+      }
+    };
   }
-}));
+};
 
-FieldValueContains = new Mongo.Collection("FieldValueContains");
-FieldValueContains.attachSchema(new SimpleSchema({
-  a: {
-    type: [String],
-    allowedValues: ["foo", "bar"]
-  },
-  b: {
-    type: String
+FieldValueContains = class FieldValueContains extends SmartModel {
+  static schema() {
+    return {
+      a: {
+        type: [String],
+        allowedValues: ["foo", "bar"]
+      },
+      b: {
+        type: String
+      }
+    };
   }
-}));
+};
 
 Template.registerHelper("currentFieldValue", function (fieldName) {
   return AutoForm.getFieldValue(fieldName) || "not selected";
